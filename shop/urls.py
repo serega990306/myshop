48d6663f337123a04cc77b698b7192f458d45b3f
serega990306 <serega990306@mail.ru>
2018-04-18 00:02:04 +1000

    Added cart dropdown menu

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,6 +8,10 @@ app_name = 'shop'
 
 urlpatterns = [
     url(r'^$', views.ProductList, name='ProductList'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
     url(r'^page/(?P<page_number>\d+)/$', views.ProductList, name='ProductList'),
     url(r'^(?P<category_slug>[-\w]+)/$', views.ProductList, name='ProductListByCategory'),
     url(r'^(?P<category_slug>[-\w]+)/page/(?P<page_number>\d+)/$', views.ProductList, name='ProductListByCategory'),
--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,77 @@ from django.shortcuts import render, get_object_or_404, render_to_response
 from .models import Category, Product, Top_Product
 from cart.forms import CartAddProductForm
 from django.core.paginator import Paginator
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render_to_response, redirect
+from django.template.context_processors import csrf
 # Страница с товарами
+
+
+def register(request):
+    content = {}
+    if request.user.is_authenticated:
+        content = {'username': auth.get_user(request).username}
+        username = content['username']
+        s = username[0] + username[1]
+        s = s.upper()
+        content['letters'] = s
+        return redirect('/profile', content)
+    content.update(csrf(request))
+    content['form'] = UserCreationForm()
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            username = request.POST.get('username', '')
+            password = request.POST.get('password1', '')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                s = username[0] + username[1]
+                s = s.upper()
+                content['letters'] = s
+                auth.login(request, user)
+                return redirect('/profile', content)
+            else:
+                content['form'] = newuser_form
+        else:
+            content['reg_error'] = 'Пароль должен быть более 8 символов и состоять из букв и цифр'
+    return render_to_response('registration.html', content)
+
+
+def login(request):
+    content = {}
+    content.update(csrf(request))
+    if request.POST:
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            content['username'] = username
+            auth.login(request, user)
+            return redirect('/profile', content)
+        else:
+            content['login_error'] = 'Пользователь не найден'
+            return render_to_response('login.html', content)
+    else:
+        return render_to_response('login.html', content)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+def profile(request):
+    content = {}
+    user = auth.get_user(request).id
+
+    if request.user.is_authenticated:
+        content = {'username': auth.get_user(request).username}
+        return render(request, 'profile.html', content)
+    else:
+        return redirect('/', content)
 
 
 def ProductList(request, category_slug=None, page_number=1):
